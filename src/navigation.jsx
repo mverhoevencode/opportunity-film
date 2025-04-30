805b81f90f7771dd2f0de167d0a7028a7e7f02ca
Mike <mike@printenbind.nl>
2025-04-30 22:49:46 +0200

    add dynamic background to nav

--- src/navigation.jsx
+++ src/navigation.jsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react";
+
 function Navigation() {
+  const [atTop, setAtTop] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setAtTop(window.scrollY <= 50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <>
-      <nav className="fixed top-0 z-10 flex w-full items-center justify-between px-8 py-4">
-        <img src="/logo.png" alt="" className="w-24" />
+      <nav
+        className={
+          "fixed top-0 z-50 flex w-full items-center justify-between px-8 py-3 transition-all duration-500 " +
+          (atTop ? "" : "bg-darkblack/50 backdrop-blur-xs")
+        }
+      >
+        <img src="/logo.png" alt="" className="w-20" />
         <ul className="flex gap-x-8">
           <li>
             <a href="javascript:;" className="cursor-pointer hover:opacity-50">
